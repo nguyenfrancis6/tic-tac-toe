fb44a2879288ed76a797ecded1f2d26e7e04701f
Francis Nguyen <nguyenfrancis6@gmail.com>
2023-09-07 00:26:33 -0700

    Tracked state of game/players

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -3,46 +3,120 @@
 const App = {
   //all of our selected html elements
   $: {
-     menu: document.querySelector('[data-id="menu"]'),
-     menuItems: document.querySelector('[data-id="menu-items"]'),
-     resetBtn: document.querySelector('[data-id="reset-btn"]'),
-     newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
-     squares: document.querySelectorAll('[data-id="square"]'),
+    menu: document.querySelector('[data-id="menu"]'),
+    menuItems: document.querySelector('[data-id="menu-items"]'),
+    resetBtn: document.querySelector('[data-id="reset-btn"]'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+    squares: document.querySelectorAll('[data-id="square"]'),
   },
 
-  init(){
-    App.registerEventListeners()
+  state: {
+    moves: [],
+  },
+
+  getGameStatus(moves) {
+    const p1Moves = moves
+      .filter((move) => move.playerId === 1)
+      .map((move) => +move.squareId);
+    const p2Moves = moves
+      .filter((move) => move.playerId === 2)
+      .map((move) => +move.squareId);
+
+    const winningPatterns = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+
+    let winner = null;
+
+    winningPatterns.forEach((pattern) => {
+      const p1Wins = pattern.every((v) => p1Moves.includes(v));
+      const p2Wins = pattern.every((v) => p2Moves.includes(v));
+
+      if (p1Wins) winner = 1;
+      if (p2Wins) winner = 2;
+    });
+
+    return {
+      status: moves.length === 9 || winner != null ? "complete" : "in-progress", // in-progress | complete
+      winner, // 1 | 2 | null
+    };
+  },
+
+  init() {
+    App.registerEventListeners();
   },
 
   registerEventListeners() {
-    App.$.menu.addEventListener('click', event => {
-      App.$.menuItems.classList.toggle('hidden')
-    })
+    App.$.menu.addEventListener("click", (event) => {
+      App.$.menuItems.classList.toggle("hidden");
+    });
 
-    App.$.resetBtn.addEventListener('click', event => {
-      console.log('reset the game')
-    })
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("reset the game");
+    });
 
-    App.$.newRoundBtn.addEventListener('click', event => {
-      console.log('Add new round')
-    })
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("Add new round");
+    });
 
-    App.$.squares.forEach(square => {
-      square.addEventListener('click', (event) => {
-        console.log(`Square with id ${event.target.id} was clicked`)
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        // Check if there is a play already
+        const hasMove = (squareId) => {
+          const existingMove = App.state.moves.find(
+            (move) => move.squareId === squareId
+          );
+          return existingMove !== undefined;
+        };
 
-        const icon = document.createElement('i')
-        icon.classList.add('fa-solid', 'fa-x', 'yellow')
+        if (hasMove(+square.id)) {
+          return;
+        }
 
-        event.target.replaceChildren(icon)
+        //Determine which player icon to add to square
 
-        // <i class="fa-solid fa-o yellow"></i>
-        // <i class="fa-solid fa-o turquoise"></i>
-      })
-    })
-  }
-}
+        const lastMove = App.state.moves.at(-1);
+        const getOppositePlayer = (playerId) => (playerId === 1 ? 2 : 1);
+        const currentPlayer =
+          App.state.moves.length === 0
+            ? 1
+            : getOppositePlayer(lastMove.playerId);
 
-window.addEventListener('load', App.init)
+        const icon = document.createElement("i");
 
+        if (currentPlayer === 1) {
+          icon.classList.add("fa-solid", "fa-x", "yellow");
+        } else {
+          icon.classList.add("fa-solid", "fa-o", "turquoise");
+        }
 
+        App.state.moves.push({
+          squareId: +square.id,
+          playerId: currentPlayer,
+        });
+
+        square.replaceChildren(icon);
+
+        //Check if there is a winner or tie
+        const game = App.getGameStatus(App.state.moves);
+
+        if (game.status === "complete") {
+          if (game.winner) {
+            alert(`Player ${game.winner} wins!`);
+          } else {
+            alert("Tie!");
+          }
+        }
+      });
+    });
+  },
+};
+
+window.addEventListener("load", App.init);
